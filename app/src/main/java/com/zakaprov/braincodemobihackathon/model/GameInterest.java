19134c5010b6b5e88f7dc9f9cb3fdc1437d77836
konrad <znfgnu@gmail.com>
2015-03-13 22:04:16 +0100

    Added BookInterest. Fixed GameInterest

--- app/src/main/java/com/zakaprov/braincodemobihackathon/model/GameInterest.java
+++ app/src/main/java/com/zakaprov/braincodemobihackathon/model/GameInterest.java
@@ -20,10 +20,10 @@ public class GameInterest extends Interest {
     @Override
     public AllegroQuery[] getQueries() {
         AllegroQuery tab[] = new AllegroQuery[4];
-        tab[1] = new AllegroQuery(this.title + " plakat", "0", "plakaty");
-        tab[2] = new AllegroQuery(this.title + " OST", "0", "soundtracki");
-        tab[3] = new AllegroQuery(this.title + " gadżet", "0", "gadżety");
-        tab[4] = new AllegroQuery(this.title + " kubek", "0", "kubki");
+        tab[0] = new AllegroQuery(this.title + " plakat", "0", "plakaty");
+        tab[1] = new AllegroQuery(this.title + " OST", "0", "soundtracki");
+        tab[2] = new AllegroQuery(this.title + " gadżet", "0", "gadżety");
+        tab[3] = new AllegroQuery(this.title + " kubek", "0", "kubki");
         return tab;
     }
 }
