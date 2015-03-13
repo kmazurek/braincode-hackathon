847a3f4b0da4fcccdd90aedc74c22f6cee79d280
Kacper20 <kacper.harasim@gmail.com>
2015-03-13 22:06:18 +0100

    band interest added

---
+++ app/src/main/java/com/zakaprov/braincodemobihackathon/model/BandInterest.java
@@ -0,0 +1,32 @@
+package com.zakaprov.braincodemobihackathon.model;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * Created by kacper on 13.03.15.
+ */
+public class BandInterest extends Interest  {
+    private String title;
+private static String[] fancyStrings = {"Blablabla fancy", "Get tickets", "So on"};
+    public BandInterest(String bandTitle) {
+        title = bandTitle;
+        this.setName(bandTitle);
+        String randomFancyText = fancyStrings[new Random().nextInt(fancyStrings.length)];
+        this.setFancyText(randomFancyText);
+        this.setImageUrl(bandTitle + ".png");
+
+    }
+
+    @Override
+    public AllegroQuery[] getQueries() {
+        AllegroQuery tab[] = new AllegroQuery[5];
+        tab[0] = new AllegroQuery(this.title + " koszulki", "0", "koszulki");
+        tab[1] = new AllegroQuery(this.title + " kostka", "0", "kostki");
+        tab[2] = new AllegroQuery(this.title + " bilet", "0", "bilety");
+        tab[3] = new AllegroQuery(this.title + " płyta winylowa", "0", "płyta");
+        tab[4] = new AllegroQuery(this.title + " kubek", "0", "kubki");
+        return tab;
+
+    }
+}
