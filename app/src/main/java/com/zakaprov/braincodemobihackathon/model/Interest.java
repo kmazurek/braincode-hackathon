ad2e5210210d0b5341622e156e70c07032374756
zakaprov <zakaprov@gmail.com>
2015-03-13 20:53:30 +0100

    Added Interest members plus getters/setters.

--- app/src/main/java/com/zakaprov/braincodemobihackathon/model/Interest.java
+++ app/src/main/java/com/zakaprov/braincodemobihackathon/model/Interest.java
@@ -2,5 +2,33 @@ package com.zakaprov.braincodemobihackathon.model;
 
 public abstract class Interest
 {
+    private String name;
+    private String imageUrl;
+    private String fancyText;
+
     public abstract AllegroQuery[] getQueries();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setFancyText(String fancyText) {
+        this.fancyText = fancyText;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getFancyText() {
+        return fancyText;
+    }
 }
