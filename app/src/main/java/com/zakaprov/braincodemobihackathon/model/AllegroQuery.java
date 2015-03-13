d9c71fc1f03ba882b2590810f9715bfe006d77b5
konrad <znfgnu@gmail.com>
2015-03-13 21:24:14 +0100

    GameInterest

--- app/src/main/java/com/zakaprov/braincodemobihackathon/model/AllegroQuery.java
+++ app/src/main/java/com/zakaprov/braincodemobihackathon/model/AllegroQuery.java
@@ -6,9 +6,16 @@ public class AllegroQuery
 {
     public String queryString;
     public String allegroCategory;
+    public String readableCategory;
 
     private void getData(NetworkCallback<Auction[]> callback)
     {
 
+    }
+
+    public AllegroQuery(String queryString, String allegroCategory, String readableCategory) {
+        this.queryString = queryString;
+        this.allegroCategory = allegroCategory;
+        this.readableCategory = readableCategory;
     }
 }
---
+++ app/src/main/java/com/zakaprov/braincodemobihackathon/model/GameInterest.java
@@ -0,0 +1,29 @@
+package com.zakaprov.braincodemobihackathon.model;
+
+import java.util.Random;
+
+/**
+ * Created by konrad on 13.03.15.
+ */
+public class GameInterest extends Interest {
+    private String title;
+    private static String[] fancyStrings = {"Get some gadgets from", "Check what's available for", "Don't you have enough"};
+
+    public GameInterest(String title) {
+        this.title = title;
+        int idx = new Random().nextInt(fancyStrings.length);
+        String random = (fancyStrings[idx]);
+        this.setFancyText(random);
+        this.setImageUrl(title + ".png");
+    }
+
+    @Override
+    public AllegroQuery[] getQueries() {
+        AllegroQuery tab[] = new AllegroQuery[4];
+        tab[1] = new AllegroQuery(this.title + " plakat", "0", "plakaty");
+        tab[2] = new AllegroQuery(this.title + " OST", "0", "soundtracki");
+        tab[3] = new AllegroQuery(this.title + " gadżet", "0", "gadżety");
+        tab[4] = new AllegroQuery(this.title + " kubek", "0", "kubki");
+        return tab;
+    }
+}
